42997d32f41071e53ca041fe5c29c174aeca1758
Emerson Silva <emerson_jdss@hotmail.com>
2022-01-10 16:41:24 -0300

    add icons for tabs and adjust tint colors and inactiveTintColors

--- src/config/tabs.js
+++ src/config/tabs.js
@@ -1,14 +1,34 @@
 import React from "react";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {ActionsStackScreen, ContactsStackScreen} from "./navigation";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ActionsStackScreen, ContactsStackScreen } from "./navigation";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const AppTabs = createBottomTabNavigator();
 
 const AppTabsScreen = () => (
-    <AppTabs.Navigator>
-        <AppTabs.Screen name="Contacts" component={ContactsStackScreen} options={{ headerShown: false}}/>
-        <AppTabs.Screen name="Actions" component={ActionsStackScreen}/>
-    </AppTabs.Navigator>
-)
+  <AppTabs.Navigator>
+    <AppTabs.Screen
+      name="Contacts"
+      component={ContactsStackScreen}
+      options={{
+        headerShown: false,
+        tabBarIcon: () => <AntDesign name="contacts" size={24} color="black" />,
+      }}
+      tabBarOptions={{
+        activeTintColor: "red",
+      }}
+    />
+    <AppTabs.Screen
+      name="Actions"
+      component={ActionsStackScreen}
+      options={{
+        tabBarIcon: () => (
+          <MaterialIcons name="assignment" size={24} color="black" />
+        ),
+      }}
+    />
+  </AppTabs.Navigator>
+);
 
 export default AppTabsScreen;
