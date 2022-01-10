f7d8d63cdd5ab9ad711c45df1559ff807ff98de3
Emerson Silva <emerson_jdss@hotmail.com>
2022-01-10 15:28:41 -0300

    adjust ward wrap

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
 import ActionsList from "../screens/ActionsList";
@@ -11,36 +11,39 @@ import AppTabsScreen from "./tabs";
 const ContactsStack = createStackNavigator();
 
 export const ContactsStackScreen = () => (
-    <ContactsStack.Navigator>
-        <ContactsStack.Screen
-            name="ContactList"
-            component={ContactsList}
-            options={{
-                title: "Contacts",
-            }}
-        />
-        <ContactsStack.Screen
-            name="ContactDetails"
-            component={ContactDetails}
-            options={({route}) => ({
-                headerTitle: `${route.params.contact.name.first} - ${route.params.contact.name.last}`,
-            })}
-        />
-    </ContactsStack.Navigator>
+  <ContactsStack.Navigator>
+    <ContactsStack.Screen
+      name="ContactList"
+      component={ContactsList}
+      options={{
+        title: "Contacts",
+      }}
+    />
+    <ContactsStack.Screen
+      name="ContactDetails"
+      component={ContactDetails}
+      options={({ route }) => ({
+        headerTitle: `${route.params.contact.name.first} - ${route.params.contact.name.last}`,
+      })}
+    />
+  </ContactsStack.Navigator>
 );
 
 const ActionsStack = createStackNavigator();
 export const ActionsStackScreen = () => (
-    <ActionsStack.Navigator>
-        <ActionsStack.Screen name="ActionsList" component={ActionsList} options={{headerShown: false}}/>
-        <ActionsStack.Screen name="ActionsDetails" component={ActionsDetails}/>
-    </ActionsStack.Navigator>
-)
-
+  <ActionsStack.Navigator>
+    <ActionsStack.Screen
+      name="ActionsList"
+      component={ActionsList}
+      options={{ headerShown: false }}
+    />
+    <ActionsStack.Screen name="ActionsDetails" component={ActionsDetails} />
+  </ActionsStack.Navigator>
+);
 
 export default () => (
-    <NavigationContainer>
-        {/*<ContactsStackScreen />*/}
-        <AppTabsScreen/>
-    </NavigationContainer>
+  <NavigationContainer>
+    {/*<ContactsStackScreen />*/}
+    <AppTabsScreen />
+  </NavigationContainer>
 );
