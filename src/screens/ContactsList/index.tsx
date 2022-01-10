475eadcc995d3e94d5b6d37a75f808877e400faa
Emerson Silva <emerson_jdss@hotmail.com>
2022-01-10 16:54:43 -0300

    remove unused imports

--- src/screens/ContactsList/index.tsx
+++ src/screens/ContactsList/index.tsx
@@ -1,5 +1,4 @@
-import { useNavigation, CommonActions } from "@react-navigation/native";
-import React, { useEffect } from "react";
+import React from "react";
 import { Text } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 
